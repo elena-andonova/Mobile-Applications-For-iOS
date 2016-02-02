7b35c2793b7cdc77f0fd16352e116f7f261c8d05
elena-andonova <ellie.andonova@gmail.com>
2016-02-02 21:50:41 +0200

    renaming project to CultureGuideApp

--- CultureGuideApp/CultureGuideApp/AppDelegate.h
+++ CultureGuideApp/CultureGuideApp/AppDelegate.h
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.h
-//  CultureGuide
+//  CultureGuideApp
 //
 //  Created by Elena Andonova on 2/2/16.
 //  Copyright © 2016 EA. All rights reserved.
--- CultureGuideApp/CultureGuideApp/ViewController.h
+++ CultureGuideApp/CultureGuideApp/ViewController.h
@@ -1,6 +1,6 @@
 //
 //  ViewController.h
-//  CultureGuide
+//  CultureGuideApp
 //
 //  Created by Elena Andonova on 2/2/16.
 //  Copyright © 2016 EA. All rights reserved.
